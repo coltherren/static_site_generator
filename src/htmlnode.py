c37dc01a2b3d817f883ce8849231e8181562234d
colt <coltonherren@gmail.com>
2024-03-13 10:15:03 -0500

    ParentNode Initialized

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -30,13 +30,35 @@ class HTMLNode:
 
 class LeafNode(HTMLNode):
 
-    def __init__(self, tag=None, value=None, props=None):
-        super().__init__(tag, value, props=props)
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
 
 
     def to_html(self):
-        if self.value == None:
-            raise ValueError("Tags must contain content (value)")
-        elif self.tag == None:
-            return f"{self.value}"
+        if self.value is None:
+            raise ValueError("Invalid HTML: no value")
+        elif self.tag is None:
+            return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+
+    def __repr__(self):
+        return f'''
+                Tag: {self.tag}
+                Value: {self.value}
+                Props: {self.props}
+        '''
+
+
+class ParentNode(HTMLNode):
+
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+
+   def to_html(self):
+       if self.tag is None:
+           raise ValueError("Invalid HTML: not tag")
+       if self.children is None:
+           raise ValueError("Invalid: ParentNodes must contain children")
+
